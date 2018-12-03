3cc21db5bda8075d96e66213178f1b8bcb58da27
EleanorHeywood <eleanor.heywood@duke.edu>
2018-12-03 15:10:46 -0500

    create merge conflict on github

--- src/runAll.R
+++ src/runAll.R
@@ -4,3 +4,4 @@ source("src/readData.R")
 source("src/wrangleData.R")
 source("src/runRegression.R")
 source("src/plotData.R")
+# HAHAHAHAHAHAH - you changed this file already in the repo and now you will have a merge conflict *** THIS text was changed on github
