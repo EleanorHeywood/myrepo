e34c28515da95a99be915849fdc2ed6a95e6d6ef
Eleanor Heywood <eleanor.heywood@duke.edu>
2018-12-03 14:34:17 -0500

    source all analysis scripts in control script

---
+++ src/runAll.R
@@ -0,0 +1,6 @@
+# File to read, wrangle, and run a regression on pilot whale data and kahuna
+
+source("src/readData.R")
+source("src/wrangleData.R")
+source("src/runRegression.R")
+source("src/plotData.R")
